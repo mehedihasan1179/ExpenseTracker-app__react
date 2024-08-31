ff5fdd04588f017edf34e2e41b65fd77af5a7cb0
mehedihasan1179 <Mehedihasan.mh111769@gmail.com>
2024-08-31 10:56:24 +0600

    first react app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import AddTransaction from './Components/AddTransaction';
+import Balance from './Components/Balance';
+import Header from './Components/Header';
+import IncomeExpenses from './Components/IncomeExpenses';
+import TransactionList from './Components/TransactionList';
+
+import {GlobalProvider} from './context/GlobalState'
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
